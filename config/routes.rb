7ad2d9c1186b4dab9779d23d596f82354a1bca52
TakashiroKazama <kazamanbou1209.pfm@gmail.com>
2025-04-06 19:29:02 +0900

    Create User API

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get :hello_world, to: "hello_world#index"
+
+      resources :users, only: %i[index]
     end
   end
 end
